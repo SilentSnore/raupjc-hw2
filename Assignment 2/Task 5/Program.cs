d1db8231357a1f216144b219f940ea426b6a878a
Dominik Pezelj <dominik.pezelj@outlook.com>
2017-11-05 17:56:27 +0100

    Task 6 and 7 completed

---
+++ Assignment 2/Task 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> results = new List<int>();
+            Parallel.For(0, 100000, (i) =>
+            {
+                Thread.Sleep(1);
+                results.Add(i * i);
+            });
+            Console.WriteLine("Bag length should be 100000. Length is {0}", results.Count);
+            
+            
+            Console.ReadLine();
+        }
+
+        public static void LongOperation(string taskName)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} Finished . Executing Thread : {1}", taskName,
+                Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ Assignment 2/Task 7/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Task_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = Task.Run(() => LetsSayUserClickedAButtonOnGuiMethodAync());
+            Console.Read();
+        }
+
+        private static async Task LetsSayUserClickedAButtonOnGuiMethodAync()
+        {
+            var result = await GetTheMagicNumberAsync();
+            Console.WriteLine(result);
+        }
+
+        private static async Task<int> GetTheMagicNumberAsync()
+        {
+            return await IKnowIGuyWhoKnowsAGuyAsync();
+        }
+
+        private static async Task<int> IKnowIGuyWhoKnowsAGuyAsync()
+        {
+            return await IKnowWhoKnowsThisAsync(10) + await IKnowWhoKnowsThisAsync(5);
+        }
+
+        private static async Task<int> IKnowWhoKnowsThisAsync(int n)
+        {
+            return await FactorialDigitSumAsync(n);
+        }
+
+        ///<sumary>
+        /// Method returns 0 for negative n or zero
+        /// </sumary>
+        public static async Task<int> FactorialDigitSumAsync(int n)
+        {
+            if (n > 0)
+            {
+                int factorial = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    factorial *= i;
+                }
+                return factorial;
+            }
+            return 0;
+        }
+    }
+}
